89359e11e28e7275b2559fbe84f687fb2752c678
Mateusz Kowalski <MateuszKowaalski@gmail.com>
2022-09-06 19:22:11 +0200

    tic tac toe

---
+++ src/TicTacToe.java
@@ -0,0 +1,124 @@
+import java.util.*;
+
+public class TicTacToe {
+
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', ' ', '+', '-'},
+                {' ', '|', '-', '|', ' '}};
+
+        printGameBorad(gameBoard);
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your placement (1-9): ");
+            int playerPosition = scanner.nextInt();
+            while (playerPositions.contains(playerPosition) || cpuPositions.contains(playerPosition)){
+                System.out.println("Position taken! Enter correct position: ");
+                playerPosition = scanner.nextInt();
+            }
+
+
+            placePiece(gameBoard, playerPosition, "player");
+
+            String result = checkWinner();
+            if (result.length() > 0){
+                System.out.println(result);
+                break;
+            }
+
+            Random random = new Random();
+            int cpuPosition = random.nextInt(9) + 1;
+            while (playerPositions.contains(cpuPosition) || cpuPositions.contains(cpuPosition)){
+                System.out.println("Position taken! Enter correct position: ");
+                cpuPosition = random.nextInt(9) + 1;
+            }
+            placePiece(gameBoard, cpuPosition, "cpu");
+
+            printGameBorad(gameBoard);
+
+            result = checkWinner();
+            if (result.length() > 0){
+                System.out.println(result);
+                break;
+            }
+        }
+
+    }
+
+    public static void printGameBorad(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos, String user) {
+
+        char symbol = ' ';
+
+        if (user.equals("player")) {
+            symbol = 'X';
+            playerPositions.add(pos);
+        } else if (user.equals("cpu")) {
+            symbol = 'O';
+            cpuPositions.add(pos);
+        }
+
+        switch (pos) {
+            case 1 -> gameBoard[0][0] = symbol;
+            case 2 -> gameBoard[0][2] = symbol;
+            case 3 -> gameBoard[0][4] = symbol;
+            case 4 -> gameBoard[2][0] = symbol;
+            case 5 -> gameBoard[2][2] = symbol;
+            case 6 -> gameBoard[2][4] = symbol;
+            case 7 -> gameBoard[4][0] = symbol;
+            case 8 -> gameBoard[4][2] = symbol;
+            case 9 -> gameBoard[4][4] = symbol;
+        }
+    }
+
+    public static String checkWinner() {
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List middleRow = Arrays.asList(4, 5, 6);
+        List bottomRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List middleCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(middleRow);
+        winning.add(bottomRow);
+        winning.add(leftCol);
+        winning.add(middleCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for (List l : winning){
+            if (playerPositions.containsAll(l)){
+                return "Congratulations you won!";
+            }if (cpuPositions.containsAll(l)){
+                return "CPU wins!";
+            }else if (playerPositions.size() + cpuPositions.size() == 9){
+                return "Draw!";
+            }
+        }
+
+        return "";
+    }
+}
